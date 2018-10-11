2fe437d11d824d06df50951040ca73de5bcf899f
Matiur Rahman Minar <minar09.bd@gmail.com>
2018-10-11 15:49:18 +0900

    fixed for starting training

--- vgg16_test.py
+++ vgg16_test.py
@@ -14,10 +14,30 @@ import os, sys
 
 
 
+
+dataset_folder = "D:/Dataset/Imagenet2012/"
+training_dataset_folder = "D:/Dataset/Imagenet2012/Images/ILSVRC2012_img_train/"
+testing_dataset_folder = "D:/Dataset/Imagenet2012/Images/ILSVRC2012_img_test/"
+validation_dataset_folder = "D:/Dataset/Imagenet2012/Images/ILSVRC2012_img_val/"
+saved_model_filepath = "./model/vgg16.ckpt"    # File path for saving the trained model
+
+
+train_label_file = 'labels.txt'
+test_label_file = 'ILSVRC2012_test_ground_truth.txt'
+validation_label_file = 'ILSVRC2012_validation_ground_truth.txt'
+
+
+f = open(train_label_file, 'r')
+training_GT = f.readlines()
+f.close()
+
+print("Number of training labels: ", len(training_GT))
+
+
 # Initialize parameters
 
 training_epochs = 30
-batch_size = 128
+batch_size = 5
 image_width = 224
 output_classes = 1000
 
@@ -29,8 +49,20 @@ def getNumber(label):
     for i, name in range(class_names):
         if name == label or label in name:
             return i
-			
-			
+            
+            
+def decode_image(pathToImage):
+    #image = imread(str(pathToImage), mode='RGB')
+    #imarray = imresize(image, (image_width, image_width))
+    imageContents = tf.read_file(str(pathToImage))
+    image = tf.image.decode_jpeg(imageContents, channels=3, dtype=tf.uint8)
+    resized_image = tf.image.resize_images(image, [image_width, image_width, 3])
+    imarray = resized_image.eval()
+    imarray = imarray.reshape(image_width, image_width, 3)
+    
+    return imarray
+            
+            
 
 x = tf.placeholder(tf.float32, shape=[None, image_width, image_width, 3])
 y = tf.placeholder(tf.float32, shape=[None, output_classes])
@@ -58,16 +90,15 @@ def main(argv):
         imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
         vgg = vgg16(imgs, "./model/vgg16.ckpt", sess)
 
-        img1 = imread(argv, mode='RGB')
-        img1 = imresize(img1, (224, 224))
+        img1 = decode_image(argv)
 
         prob = sess.run(vgg.probs, feed_dict={vgg.imgs: [img1]})[0]
         preds = (np.argsort(prob)[::-1])[0:5]
         for p in preds:
             print(class_names[p], prob[p])
-	
+    
     
 if __name__ == "__main__":
     main(sys.argv[1])
-			
-			
+            
+            
--- vgg16_train.py
+++ vgg16_train.py
@@ -5,6 +5,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
+
 import tensorflow as tf
 import numpy as np
 from random import randint
@@ -14,6 +15,7 @@ import vgg16
 import os, os.path, time
 
 
+
 dataset_folder = "D:/Dataset/Imagenet2012/"
 training_dataset_folder = "D:/Dataset/Imagenet2012/Images/ILSVRC2012_img_train/"
 testing_dataset_folder = "D:/Dataset/Imagenet2012/Images/ILSVRC2012_img_test/"
@@ -21,80 +23,132 @@ validation_dataset_folder = "D:/Dataset/Imagenet2012/Images/ILSVRC2012_img_val/"
 saved_model_filepath = "./model/vgg16.ckpt"    # File path for saving the trained model
 
 
+train_label_file = 'labels.txt'
 test_label_file = 'ILSVRC2012_test_ground_truth.txt'
 validation_label_file = 'ILSVRC2012_validation_ground_truth.txt'
+
+
+f = open(train_label_file, 'r')
+training_GT = f.readlines()
+f.close()
+
+print("Number of training labels: ", len(training_GT))
 
 f = open(test_label_file, 'r')
 testing_GT = f.readlines()
 f.close()
 
+print("Number of testing images: ", len(testing_GT))
+
 f = open(validation_label_file, 'r')
 validation_GT = f.readlines()
 f.close()
 
+print("Number of validation images: ", len(validation_GT))
 
-# Initialize parameters
-
-training_epochs = 10
-batch_size = 128
-image_width = 224
-output_classes = 1000
-
-learning_rate = 0.001
 
 training_dataset_size = len(os.listdir(training_dataset_folder))
 testing_dataset_size = len(os.listdir(testing_dataset_folder))
+validation_dataset_size = len(os.listdir(validation_dataset_folder))
+
+#print("Number of training image folders: ", training_dataset_size)
+
+
+# Initialize parameters
+training_epochs = 10
+batch_size = 5
+image_width = 224
+input_depth = 3
+output_classes = len(training_GT)
+
+learning_rate = 0.001
+
+
+#print("Testing images: ", testing_dataset_size)
+#print("Validation images: ", validation_dataset_size)
 
 class_folders = os.listdir(training_dataset_folder)
+for each in class_folders:
+    if ".tar" in each:
+        class_folders.remove(each)
+print("Training folders : ", len(class_folders))
 
+
+def getNumber(label):
+    return np.eye(output_classes, dtype=np.float32)[int(label)]
+    
+
+def decode_image(pathToImage):
+    #image = imread(str(pathToImage), mode='RGB')
+    #imarray = imresize(image, (image_width, image_width))
+    imageContents = tf.read_file(str(pathToImage))
+    image = tf.image.decode_jpeg(imageContents, channels=input_depth)
+    
+    resized_image = tf.image.resize_images(image, [image_width, image_width])
+    imarray = resized_image.eval()
+    imarray = imarray.reshape(image_width, image_width, input_depth)
+    
+    return imarray
+    
               
 # Return the batch of training data for next run
 def get_next_batch_of_training_images(class_folder, imagesPathArray, class_number):
      
-    dataset = np.ndarray(shape=(0, image_width, image_width, 3), dtype=np.float32)
+    dataset = np.ndarray(shape=(0, image_width, image_width, input_depth), dtype=np.float32)
     labels = np.ndarray(shape=(0, output_classes), dtype=np.float32)
     
     for i in range(len(imagesPathArray)):
 
         try:
-            pathToImage = training_dataset_folder + class_folder + imagesPathArray[i]
-            image = imread(str(pathToImage), mode='RGB')
-            imarray = imresize(image, (image_width, image_width))
+            pathToImage = training_dataset_folder + class_folder + "/" + imagesPathArray[i]
+            #print(pathToImage)
+            
+            imarray = decode_image(pathToImage)
+            
             appendingImageArray = np.array([imarray], dtype=np.float32)
-            appendingNumberLabel = np.array([class_number], dtype=np.float32)
+            appendingNumberLabel = np.array([getNumber(class_number)], dtype=np.float32)
+            
+            #print(appendingImageArray.shape, appendingNumberLabel.shape)
+            
             dataset = np.append(dataset, appendingImageArray, axis=0)
             labels = np.append(labels, appendingNumberLabel, axis=0)
-        except:
-            print("Unexpected image - ", imagesPathArray[i], ", skipping...")
+			
+        except Exception as err:
+            print("Unexpected image - ", imagesPathArray[i], ", skipping...", err)
             
     return dataset, labels
             
             
             
-def get_testing_images(dataset_folder, imagesPathArray, labels_array):
+def get_testing_images(imagesPathArray, labels_array, type="testing"):
 
-    dataset = np.ndarray(shape=(0, image_width, image_width, 3), dtype=np.float32)
+    dataset = np.ndarray(shape=(0, image_width, image_width, input_depth), dtype=np.float32)
     labels = np.ndarray(shape=(0, output_classes), dtype=np.float32)
     
     for i in range(len(imagesPathArray)):
 
         try:
             pathToImage = testing_dataset_folder + imagesPathArray[i]
-            image = imread(str(pathToImage), mode='RGB')
-            imarray = imresize(image, (image_width, image_width))
+            if type == "validation":
+                pathToImage = validation_dataset_folder + imagesPathArray[i]
+                
+            imarray = decode_image(pathToImage)
+            
             appendingImageArray = np.array([imarray], dtype=np.float32)
-            appendingNumberLabel = np.array([labels_array[i]], dtype=np.float32)
+            appendingNumberLabel = np.array([getNumber(labels_array[i])], dtype=np.float32)
+            
             dataset = np.append(dataset, appendingImageArray, axis=0)
             labels = np.append(labels, appendingNumberLabel, axis=0)
-        except:
-            print("Unexpected image - ", imagesPathArray[i], ", skipping...")
+        
+        except Exception as err:
+            print("Unexpected image - ", imagesPathArray[i], " skipping...", err)
             
     return dataset, labels
     
 
             
 # Initialize input and output
-x = tf.placeholder(tf.float32, shape=[None, image_width, image_width, 3])
+x = tf.placeholder(tf.float32, shape=[None, image_width, image_width, input_depth])
 y = tf.placeholder(tf.float32, shape=[None, output_classes])
 
 
@@ -135,56 +189,99 @@ if __name__ == '__main__':
         # Starting time
         t1 = time.time()
         
-        training_steps = round(training_dataset_size / batch_size)
-        testing_steps = round(testing_dataset_size / batch_size)
+        # test and validation images
+        test_image_names = os.listdir(testing_dataset_folder)
+        val_image_names = os.listdir(validation_dataset_folder)
         
         
         for epoch in range(1, training_epochs + 1):
         
             train_accuracy_list = []
+            test_accuracy_list = []
             loss_list = []
             
+            print("\n")
             print("Starting epoch: ", epoch)
             
-        
+            
             for i in range(training_dataset_size):
-                try:
-                    image_names = os.listdir(training_dataset_folder + class_folder[i])
-                    batch_train_images, batch_train_labels = get_next_batch_of_training_images(class_folder[i], image_names, int(class_number[i])+1)
-                    training_data = {x: batch_train_images, y: batch_train_labels}
-                    sess.run(optimizer, feed_dict=training_data)
+            
+                training_path = training_dataset_folder + class_folders[i] + "/"
+                print("Training folder path: ", training_path)
+                image_names = os.listdir(training_path)
+                print("Number of images in this folder: ", len(image_names))
+                this_class_label = i + 1
+                #print(this_class_label)
+                
+                start_index = 0
+                num_images = len(image_names)
+                num_steps = round(num_images/batch_size)
+                
+            
+                for j in range(num_steps):
+            
+                    try:
                     
-                    # logging
-                    train_accuracy = accuracy.eval(feed_dict=training_data)
-                    loss_print = loss.eval(feed_dict=training_data)
-                    train_accuracy_list.append(train_accuracy)
-                    loss_list.append(loss_print)
-                except:
-                    print("Something is wrong in training!")
+                        last_index = start_index + (batch_size - 1)
                     
+                        step_images = image_names[start_index:last_index]
                     
+                        batch_train_images, batch_train_labels = get_next_batch_of_training_images(class_folders[i], image_names, this_class_label)
+                        training_data = {x: batch_train_images, y: batch_train_labels}
+                        sess.run(optimizer, feed_dict=training_data)
+                        
+                        # logging
+                        train_accuracy = accuracy.eval(feed_dict=training_data)
+                        loss_print = loss.eval(feed_dict=training_data)
+                        
+                        train_accuracy_list.append(train_accuracy)
+                        loss_list.append(loss_print)
+                        
+                    except Exception as err:
+                        print("Error in training! Epoch:", epoch, " , Folder:", i, ", Step:", j, err)            
+
+                    start_index = start_index + batch_size
+                    
+                        
             training_accuracy = np.mean(train_accuracy_list)
             loss_here = np.mean(loss_list)
             
             writer_train.add_summary(sess.run(merged_history, feed_dict={accuracy_history: training_accuracy}), epoch)
             writer_loss.add_summary(sess.run(merged_history, feed_dict={accuracy_history: loss_here}), epoch)
-                    
-
-            try:
-                image_names = os.listdir(testing_dataset_folder)
-                batch_test_images, batch_test_labels = get_testing_images(testing_dataset_folder, image_names, testing_GT)
-                testing_data = {x: batch_test_images, y: batch_test_labels}
-                
-                # logging
-                test_accuracy = accuracy.eval(feed_dict=testing_data)
-                writer_test.add_summary(sess.run(merged_history, feed_dict={accuracy_history: testing_accuracy}), epoch)
-            except:
-                print("Something is wrong in testing!")
+        
             
+            
+            start_index = 0
+            num_images = len(test_image_names)
+            num_steps = round(num_images/batch_size)
+            
+        
+            for k in range(num_steps):            
+                try:
+                    last_index = start_index + (batch_size - 1)
+                    step_images = image_names[start_index:last_index]
+                    step_labels = testing_GT[start_index:last_index]
+                    
+                    batch_test_images, batch_test_labels = get_testing_images(step_images, step_labels, "testing")
+                    testing_data = {x: batch_test_images, y: batch_test_labels}
+                    
+                    # logging
+                    test_accuracy = accuracy.eval(feed_dict=testing_data)
+                    test_accuracy_list.append(test_accuracy)
+                    
+                except Exception as err:
+                    print("Error in testing step:", k, err)
+                    
+                start_index = start_index + batch_size
+            
+            
+            testing_accuracy = np.mean(test_accuracy_list)
+            writer_test.add_summary(sess.run(merged_history, feed_dict={accuracy_history: testing_accuracy}), epoch)
             
             print("Training accuracy: ", training_accuracy * 100, ", Loss: ", loss_here, "%, Testing accuracy: ", testing_accuracy * 100, "%, ", epoch, "th epoch.")
             
                 
+        print("\n")
         # Ending time
         t2 = time.time()
                 
@@ -193,21 +290,37 @@ if __name__ == '__main__':
         print("Model saved at: " ,savedPath)
         
             
-    print("Learning time: " + str(t2-t1) + " seconds")
-    
-    
-    try:
-        val_image_names = os.listdir(validation_dataset_folder)
-        batch_test_images, batch_test_labels = get_testing_images(validation_dataset_folder, val_image_names, validation_GT)
-        val_data = {x: batch_test_images, y: batch_test_labels}
+        print("Learning time: " + str(t2-t1) + " seconds")
         
-        # logging
-        validation_accuracy = accuracy.eval(feed_dict=val_data)
         
-        print("Validation accuracy: ", validation_accuracy * 100)
-    except:
-        print("Something is wrong in testing!")
+        val_acc_list = []
+        start_index = 0
+        num_images = len(val_image_names)
+        num_steps = round(num_images/batch_size)
+            
+            
+        for m in range(num_steps):            
+            try:
+                last_index = start_index + (batch_size - 1)
+                step_images = image_names[start_index:last_index]
+                step_labels = validation_GT[start_index:last_index]
 
+                
+                batch_val_images, batch_val_labels = get_testing_images(step_images, step_labels, "validation")
+                val_data = {x: batch_val_images, y: batch_val_labels}
+                
+                # logging
+                val_acc = accuracy.eval(feed_dict=val_data)
+                val_acc_list.append(val_acc)
+                
+            except Exception as err:
+                print("Error in validation step:", m, err)
+                
+            start_index = start_index + batch_size
+
+        validation_accuracy = np.mean(val_acc_list)
+        print("Validation accuracy: ", validation_accuracy * 100)
+            
 
     
 
